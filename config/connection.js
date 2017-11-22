4cc428d407fb0cbddfb6a21254c1b51eef14d14c
andalibkibria <andalib.kibria@gmail.com>
2017-11-21 20:43:26 -0500

    Wireframe

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 8080,
+  user: 'root',
+  password: '?????????????',
+  database:'school_db'
+});
+
+module.exports = connection;
