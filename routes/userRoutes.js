ceef8c812798e6c6faef4cb8222830433a120253
arigold24k <ariel.m.vidal@gmail.com>
2017-12-03 18:08:29 -0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,7 @@
 const authentication =  require('../controllers/authenticationController');
+var parent = require("../fucntions/parent");
+
+
 const router = require('express').Router();
 
 router.get('/', function (req, res) {
@@ -9,6 +12,24 @@ router.get('/register', function (req, res) {
   res.render('register')
 })
 
+router.get("/reportcard", function(req, res) {
+//call function from teacherorm
+        parent.retrieve(function (data) {
+            var average = ((Number(data[0].math) + Number(data[0].reading) + Number(data[0].socialstudies) + Number(data[0].science)) / 4);
+            var hbsObject = {
+                name: data[0].username,
+                math: data[0].math,
+                reading: data[0].reading,
+                socialstudies: data[0].socialstudies,
+                science: data[0].science,
+                average: average
+            };
+            console.log(data);
+            console.log(hbsObject);
+            res.render("reportcard", hbsObject);
+        });
+
+    });
 
 router.get('/chat', function (req, res) {
   res.render('chat')
